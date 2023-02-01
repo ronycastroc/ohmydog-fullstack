e9a6be391e24ffb3e7e019bb86168a0ce3ecde0d
ronycastroc <castroroony@gmail.com>
2023-02-01 17:13:28 -0300

    feat: tests update dog route

--- tests/integration/dogs.test.ts
+++ tests/integration/dogs.test.ts
@@ -19,6 +19,14 @@ beforeEach(async () => {
 
 const server = supertest(app);
 
+const generateValidBody = () => ({
+  name: faker.name.firstName(),
+  age: ages.Filhote,
+  genre: genres.Macho,
+  description: faker.lorem.word(),
+  urlImage: faker.internet.url(),
+});
+
 describe("POST /dogs", () => {
   it("should respond with status 401 if no token is given", async () => {
     const response = await server.post("/dogs");
@@ -44,14 +52,6 @@ describe("POST /dogs", () => {
   });
 
   describe("when token is valid", () => {
-    const generateValidBody = () => ({
-      name: faker.name.firstName(),
-      age: ages.Filhote,
-      genre: genres.Macho,
-      description: faker.lorem.word(),
-      urlImage: faker.internet.url(),
-    });
-
     it("should respond with status 422 when body is not given", async () => {
       const user = await createUser();
       const token = await generateValidToken(user);
@@ -204,6 +204,116 @@ describe("GET /dogs/:dogId", () => {
         urlImage: dog.urlImage,
         createdAt: dog.createdAt.toISOString(),
         updatedAt: dog.updatedAt.toISOString(),
+      });
+    });
+  });
+});
+
+describe("PUT /dogs/:dogId", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.put("/dogs/1");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.put("/dogs/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.put("/dogs/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 422 when body is not given", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const dog = await createDog({ userId: user.id });
+
+      const response = await server.put(`/dogs/${dog.id}`).set("Authorization", `Bearer ${token}`);
+  
+      expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+    });
+  
+    it("should respond with status 422 when body is not valid", async () => {
+      const invalidBody = { [faker.lorem.word()]: faker.lorem.word() };
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const dog = await createDog({ userId: user.id });
+  
+      const response = await server.put(`/dogs/${dog.id}`).set("Authorization", `Bearer ${token}`).send(invalidBody);
+  
+      expect(response.status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+    });
+    
+    it("should respond with status 400 when when url image is not valid", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const dog = await createDog({ userId: user.id });
+      const urlImageInvalid = { 
+        name: faker.name.firstName(),
+        age: ages.Filhote,
+        genre: genres.Macho,
+        description: faker.lorem.word(),
+        urlImage: faker.lorem.text()
+      };
+  
+      const response = await server.put(`/dogs/${dog.id}`).set("Authorization", `Bearer ${token}`).send(urlImageInvalid);
+  
+      expect(response.status).toBe(httpStatus.BAD_REQUEST);
+    });
+
+    it("should respond with status 404 for invalid dog id", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createDog({ userId: user.id });
+      const dogBody = generateValidBody();
+
+      const response = await server.put("/dogs/0").set("Authorization", `Bearer ${token}`).send(dogBody);
+  
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });   
+
+    it("should respond with status 401 when body is valid but account type is not valid", async () => {
+      const user = await createUser();
+      const user2 = await createUser({ accountType: accounts.Membro });
+      const token = await generateValidToken(user2);
+      const dog = await createDog({ userId: user.id });
+      const dogBody = generateValidBody();
+
+      const response = await server.put(`/dogs/${dog.id}`).set("Authorization", `Bearer ${token}`).send(dogBody);
+  
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });   
+
+    it("should respond with status 200 and dog data", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const dog = await createDog({ userId: user.id });
+      const dogBody = generateValidBody();
+
+      const response = await server.put(`/dogs/${dog.id}`).set("Authorization", `Bearer ${token}`).send(dogBody);
+
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toEqual({
+        id: dog.id,
+        userId: dog.userId,
+        name: dogBody.name,
+        age: dogBody.age,
+        genre: dogBody.genre,
+        description: dogBody.description,
+        urlImage: dogBody.urlImage,
+        createdAt: dog.createdAt.toISOString(),
+        updatedAt: expect.any(String),
       });
     });
   });
