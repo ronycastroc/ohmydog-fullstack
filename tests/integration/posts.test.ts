4a3b657538d45b95eed1a0508b9e3e76a17df1b3
ronycastroc <castroroony@gmail.com>
2023-02-01 20:56:44 -0300

    feat: tests get post by id route

--- tests/integration/posts.test.ts
+++ tests/integration/posts.test.ts
@@ -120,3 +120,63 @@ describe("GET /posts", () => {
     ]);
   });
 });
+
+describe("GET /posts/:postId", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    const response = await server.get("/posts/1");
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if given token is not valid", async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.get("/posts/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if there is no session for given token", async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.get("/posts/1").set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 404 when for invalid dog id", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      await createPost({ userId: user.id });
+
+      const response = await server.get("/posts/0").set("Authorization", `Bearer ${token}`);
+  
+      expect(response.status).toBe(httpStatus.NOT_FOUND);
+    });   
+
+    it("should respond with status 200 and dog data", async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const post = await createPost({ userId: user.id });
+
+      const response = await server.get(`/posts/${post.id}`).set("Authorization", `Bearer ${token}`).send(post);
+
+      expect(response.status).toBe(httpStatus.OK);
+      expect(response.body).toEqual({
+        id: post.id,
+        userId: post.userId,
+        title: post.title,
+        text: post.text,
+        isUpdated: post.isUpdated,
+        createdAt: post.createdAt.toISOString(),
+        updatedAt: post.updatedAt.toISOString(),
+        comments: [],
+        _count: {
+          stars: 0,
+        }
+      });
+    });
+  });
+});
