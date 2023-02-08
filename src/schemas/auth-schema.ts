86772e4b36a177c278de8b7c7e95461f28b244db
ronycastroc <castroroony@gmail.com>
2023-02-07 23:51:33 -0300

    chore: auth schema

--- src/schemas/auth-schema.ts
+++ src/schemas/auth-schema.ts
@@ -3,5 +3,5 @@ import joi from "joi";
 
 export const authSchema = joi.object<SignInParams>({
   email: joi.string().email().required(),
-  password: joi.string().min(6).max(100).required(),
+  password: joi.string().required(),
 });
