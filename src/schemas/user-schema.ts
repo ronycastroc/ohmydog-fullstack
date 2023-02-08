959e8a5ded0017dbb38d6922e120ae2eefd072ab
ronycastroc <castroroony@gmail.com>
2023-02-08 00:28:28 -0300

    chore: sign-up schema

--- src/schemas/user-schema.ts
+++ src/schemas/user-schema.ts
@@ -2,7 +2,7 @@ import { CreateUserParams } from "@/service/user-service";
 import joi from "joi";
 
 export const createUserSchema = joi.object<CreateUserParams>({
-  name: joi.string().min(2).max(50).required(),
+  name: joi.string().min(1).max(50).required(),
   email: joi.string().email().required(),
   password: joi.string().min(6).max(100).required(),
   urlImage: joi.string().required(),
