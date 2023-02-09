7bc932ec4a97ddcd350e5b66dd7479cb6be9482d
ronycastroc <castroroony@gmail.com>
2023-02-08 21:39:59 -0300

    style: footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,39 @@
 import styled from "styled-components";
 import omdLogo from "../../assets/images/ohmydog-underline.png";
-import { BsInstagram, BsFacebook, BsTwitter, BsYoutube } from "react-icons/bs";
-import { IconContext } from "react-icons";
+import { BsInstagram, BsFacebook, BsTwitter, BsYoutube, BsGeoAlt, BsTelephone, } from "react-icons/bs";
+import { IoMailOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
 export const Footer = () => {
   const navigate = useNavigate();
   return (
     <Wrapper>
+      <Contact>
+        <div>
+          <BsGeoAlt className="icon-info" />
+          <span>Cardiff, West Grove, CF24 3AN</span>
+        </div>
+        <div>
+          <BsTelephone className="icon-info" />
+          <span>+44 (29) 555-5555</span>
+        </div>
+        <div>
+          <IoMailOutline className="icon-info" />
+          <span>ohmydog@email.com</span>
+        </div>
+      </Contact>
+      <LogoFooter>
+        <img src={omdLogo} alt="" onClick={() => navigate("/")} />
+        <p>&copy; Since 2023 All Rights Reserved</p>
+      </LogoFooter>
       <SocialMedia>
-        <img src={omdLogo} alt="" onClick={() => navigate("/")}/>
-        <IconContext.Provider value={{
-          color: "var(--white-color)",
-          className: "global-class-name",
-          size: "1.6vw",
-        }}>
-          <div>
-            <BsInstagram style={{cursor:"pointer"}}/>
-            <BsFacebook style={{cursor:"pointer"}}/>
-            <BsTwitter style={{cursor:"pointer"}}/>
-            <BsYoutube style={{cursor:"pointer"}}/>
-          </div>
-        </IconContext.Provider>
-        
+        <h1>SOCIAL MEDIA</h1>
+        <div>
+          <BsInstagram className="icon" />
+          <BsFacebook className="icon" />
+          <BsTwitter className="icon" />
+          <BsYoutube className="icon" />
+        </div>
       </SocialMedia>
     </Wrapper>
   );
@@ -37,14 +48,16 @@ const Wrapper = styled.div`
   position: relative;
   bottom: 0;
   left: 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
-const SocialMedia = styled.div`
-  width: 13%;
-  height: 75%;
+const LogoFooter = styled.div`
+  width: 20%;
+  height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   
   img {
@@ -53,9 +66,60 @@ const SocialMedia = styled.div`
     cursor: pointer;
   }
 
+  p {
+    text-align: center;
+    color: var(--white-color)
+  }
+`;
+
+const Contact = styled.div`
+  width: 20%;
+  line-height: 2.2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  
+
+  .icon-info {
+    color: var(--white-color);
+    font-size: 1.3rem;
+  }
+  
   div {
-    width: 100%;
+    display: flex;    
+    align-items: center;
+    cursor: pointer;
+  }
+
+  span {
+    margin-left: 5px;
+    color: var(--white-color);
+    font-size: 0.9rem;
+  }
+`;
+
+const SocialMedia = styled.div`
+  width: 20%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  h1 {
+    text-align: center;
+    font-size: 1.4rem;
+    font-weight: 500;
+    color: var(--white-color);
+  }
+
+  div {
     display: flex;
     justify-content: space-evenly;
+  }
+
+  .icon {
+    color: var(--white-color);
+    font-size: 1.6rem;
+    cursor: pointer;
   }
 `;
